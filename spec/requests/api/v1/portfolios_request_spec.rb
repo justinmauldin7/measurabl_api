c0efe7a40b91410f2ecaf28cb2b6e24ba670aa95
Justin Mauldin <justin.mauldin7@gmail.com>
2022-10-28 14:53:49 -0600

    Adds the test to the "portfolios_request_spec.rb" file for getting a single portfolio record.

--- spec/requests/api/v1/portfolios_request_spec.rb
+++ spec/requests/api/v1/portfolios_request_spec.rb
@@ -13,4 +13,33 @@ describe 'Portfolios API' do
     expect(response).to be_successful
     expect(Portfolio.count).to eq(5)
   end
+
+  it "can get a single portfolio" do
+    portfolio_1 = Portfolio.create(portfolio_name: "United States Fund", company_name: "ACME Inc.", company_size: 1000, country: "United States")
+
+    get "/api/v1/portfolios/#{portfolio_1.id}"
+
+    portfolio = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(portfolio).to have_key(:id)
+    expect(portfolio[:id]).to eq(portfolio_1.id)
+
+    expect(portfolio).to have_key(:portfolio_name)
+    expect(portfolio[:portfolio_name]).to be_a(String)
+    expect(portfolio[:portfolio_name]).to eq(portfolio_1.portfolio_name)
+
+    expect(portfolio).to have_key(:company_name)
+    expect(portfolio[:company_name]).to be_a(String)
+    expect(portfolio[:company_name]).to eq(portfolio_1.company_name)
+
+    expect(portfolio).to have_key(:company_size)
+    expect(portfolio[:company_size]).to be_an(Integer)
+    expect(portfolio[:company_size]).to eq(portfolio_1.company_size)
+
+    expect(portfolio).to have_key(:country)
+    expect(portfolio[:country]).to be_a(String)
+    expect(portfolio[:country]).to eq(portfolio_1.country)
+  end
 end
