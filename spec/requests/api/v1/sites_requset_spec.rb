71984473d00ff9ee16b0dfdc71dcc2732f5890c5
Justin Mauldin <justin.mauldin7@gmail.com>
2022-10-28 16:32:37 -0600

    Creates the api requests test to confirm the API endpoint can return a single site record.

--- spec/requests/api/v1/sites_requset_spec.rb
+++ spec/requests/api/v1/sites_requset_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 
 describe 'Sites API' do
+  it "can get a single site" do
+    site_1 = Site.create(site_name: "Larimer Plaza", site_address: "123 Main St. Denver, CO 80203", building_type: "office building")
+
+    get "/api/v1/sites/#{site_1.id}"
+
+    site = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(site).to have_key(:id)
+    expect(site[:id]).to eq(site_1.id)
+
+    expect(site).to have_key(:site_name)
+    expect(site[:site_name]).to be_a(String)
+    expect(site[:site_name]).to eq(site_1.site_name)
+
+    expect(site).to have_key(:company_name)
+    expect(site[:site_address]).to be_a(String)
+    expect(site[:site_address]).to eq(site_1.site_address)
+
+    expect(site).to have_key(:building_type)
+    expect(site[:building_type]).to be_a(String)
+    expect(site[:building_type]).to eq(site_1.building_type)
+  end
 end
