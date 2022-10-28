df684801e55a076ed459d1e3c82a00bbb77e7cc7
Justin Mauldin <justin.mauldin7@gmail.com>
2022-10-28 16:53:01 -0600

    Creates the relationship model test to validate a site should have the relationship of belonging to portfolios.

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe Site do
+  describe 'relationships' do
+    it { should belong_to(:portfolio) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:site_name) }
     it { should validate_presence_of(:site_address) }
