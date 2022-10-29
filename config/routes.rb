d0b643e8f348cab7631557719a6511467fb60ba2
Justin Mauldin <justin.mauldin7@gmail.com>
2022-10-28 21:07:49 -0600

    Creates the "/api/v1/spaces" index route to return all spaces associated with a site.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :portfolios, only: [:index, :show]
       resources :sites, only: [:index, :show]
-      resources :spaces, only: [:show]
+      resources :spaces, only: [:index, :show]
     end
   end
 end
