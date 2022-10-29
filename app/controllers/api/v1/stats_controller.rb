ef6d83ed045ace8b12040539d4f279aa2a76e024
Justin Mauldin <justin.mauldin7@gmail.com>
2022-10-28 22:35:03 -0600

    Adds the private helper method "create_payload" to the Api::V1::StatsController for having a different payload depending on what type of id is passed through.

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -1,4 +1,27 @@
 class Api::V1::StatsController < ApplicationController
   def index
   end
+
+  private
+
+  def create_payload(record, total_square_footage, average_suare_footage)
+    if record.class == Site
+      payload = {
+        site_id: record.id,
+        site_name: record.site_name,
+        total_square_footage: total_square_footage,
+        average_suare_footage: average_suare_footage
+      }
+    elsif record.class == Portfolio
+      payload = {
+        portfolio_id: record.id,
+        portfolio_name: record.portfolio_name,
+        sites: record.sites.pluck("site_name"),
+        total_square_footage: total_square_footage,
+        average_suare_footage: average_suare_footage
+      }
+    end
+
+    return payload
+  end
 end
