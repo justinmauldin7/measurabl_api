4cc1f40c5c752d33600fa5fd43c5460d60b79df2
Justin Mauldin <justin.mauldin7@gmail.com>
2022-10-28 20:05:06 -0600

    Creates the "validations" section of the Site's model tests to validate the information/fields that should exist for it.

--- spec/models/spaces_spec.rb
+++ spec/models/spaces_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 describe Site do
+  describe 'validations' do
+    it { should validate_presence_of(:space_name) }
+    it { should validate_presence_of(:property_type) }
+    it { should validate_presence_of(:site_size) }
+    it { should validate_presence_of(:monthly_price) }
+  end
 end
