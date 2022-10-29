593cd6a924f1ee272edbb804c3d9b342000c2a98
Justin Mauldin <justin.mauldin7@gmail.com>
2022-10-28 20:18:56 -0600

    Fixes test to validate presence of "space_size" instead of "site_size" for less confusion.

--- spec/models/spaces_spec.rb
+++ spec/models/spaces_spec.rb
@@ -4,7 +4,7 @@ describe Space do
   describe 'validations' do
     it { should validate_presence_of(:space_name) }
     it { should validate_presence_of(:property_type) }
-    it { should validate_presence_of(:site_size) }
+    it { should validate_presence_of(:space_size) }
     it { should validate_presence_of(:monthly_price) }
   end
 end
