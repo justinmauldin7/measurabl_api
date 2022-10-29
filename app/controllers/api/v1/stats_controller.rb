9402fb625d0bb970d36c05727a56225001b75887
Justin Mauldin <justin.mauldin7@gmail.com>
2022-10-28 22:35:40 -0600

    Adds the logic & math needed to get the average & total square feet for all spaces on a single site or a single portfolio.

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::StatsController < ApplicationController
   def index
+    if params[:site_id]
+      record = Site.find_by_id(params[:site_id])
+    elsif params[:portfolio_id]
+      record = Portfolio.find_by_id(params[:portfolio_id])
+    end
+
+    space_sizes_array = record.spaces.pluck("space_size")
+
+    total_square_footage = space_sizes_array.sum
+
+    average_suare_footage = total_square_footage / space_sizes_array.size
+
+    render json: create_payload(record, total_square_footage, average_suare_footage)
   end
 
   private
