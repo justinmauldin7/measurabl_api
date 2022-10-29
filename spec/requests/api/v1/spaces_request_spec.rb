261ea14008d19c57226612fdbec6f103b6c3b904
Justin Mauldin <justin.mauldin7@gmail.com>
2022-10-28 21:06:19 -0600

    In the spaces return single record API request test, adds the creation of the portfolio & site and adds expecs assertions for it.

--- spec/requests/api/v1/spaces_request_spec.rb
+++ spec/requests/api/v1/spaces_request_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 describe 'Spaces API' do
   it "can get a single space" do
-    space_1 = Space.create(space_name: "Suite C", property_type: "commercial", space_size: 2000, monthly_price: 100000)
+    portfolio_1 = Portfolio.create(portfolio_name: "United States Fund", company_name: "ACME Inc.", company_size: 1000, country: "United States")
+
+    site_1 = Site.create(portfolio: portfolio_1, site_name: "Larimer Plaza", site_address: "123 Main St. Denver, CO 80203", building_type: "office building")
+
+    space_1 = Space.create(site: site_1, space_name: "Suite C", property_type: "commercial", space_size: 2000, monthly_price: 100000)
 
     get "/api/v1/spaces/#{space_1.id}"
 
@@ -12,6 +16,9 @@ describe 'Spaces API' do
 
     expect(space).to have_key(:id)
     expect(space[:id]).to eq(space_1.id)
+
+    expect(space).to have_key(:site_id)
+    expect(space[:site_id]).to eq(site_1.id)
 
     expect(space).to have_key(:space_name)
     expect(space[:space_name]).to be_a(String)
