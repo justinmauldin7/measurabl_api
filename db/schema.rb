a76424ffd0c83a5c92c58591c75731a0174ada52
Justin Mauldin <justin.mauldin7@gmail.com>
2022-10-28 20:59:02 -0600

    Creates the migration & updates the schema to have the spaces model be associated with the sites table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_29_022720) do
+ActiveRecord::Schema.define(version: 2022_10_29_025647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,10 @@ ActiveRecord::Schema.define(version: 2022_10_29_022720) do
     t.integer "monthly_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "site_id"
+    t.index ["site_id"], name: "index_spaces_on_site_id"
   end
 
   add_foreign_key "sites", "portfolios"
+  add_foreign_key "spaces", "sites"
 end
