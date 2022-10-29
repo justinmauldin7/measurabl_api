dc4e64304d64bf28bccd6ff7bb157ef090cb65ef
Justin Mauldin <justin.mauldin7@gmail.com>
2022-10-28 21:03:22 -0600

    Adds the relationships validation to below to site in the "spaces_spec.rb" file.

--- spec/models/spaces_spec.rb
+++ spec/models/spaces_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe Space do
+  describe 'relationships' do
+    it { should belong_to(:site) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:space_name) }
     it { should validate_presence_of(:property_type) }
